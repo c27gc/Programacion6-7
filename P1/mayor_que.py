ae7df5a1bcaa4954380c0298efd8aaa419d411db
cegc <gcarloseduardo1@gmail.com>
2019-05-23 21:56:57 -0400

    Código Sesión Práctica 1

--- P1/mayor_que.py
+++ P1/mayor_que.py
@@ -1,8 +1,8 @@
 #Programa desarrollado en clases práctica de la sección 07 de programación UCV
 #Este programa obtiene determina cual de dos números introducidos por el usuario
 #es mayor
-num1 = input("Introduzca el primer Número")
-num2 = input("Introduzca el segundo Número")
+num1 = int(input("Introduzca el primer Número: "))
+num2 = int(input("Introduzca el segundo Número: "))
 
 if num1 > num2:
     print("El número {} es mayor que {}".format(num1,num2))
--- P1/paridad.py
+++ P1/paridad.py
@@ -1,6 +1,6 @@
 #Programa desarrollado en clases práctica de la sección 06 de programación UCV
 #Este programa determina si un número introducido por el usuario es par o no
-num = input("Introduzca el Número")
+num = int(input("Introduzca el Número: "))
 
 k = num % 2
 if k == 0:
